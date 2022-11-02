eafef62db3337683d3ca628b0359ce52abc1a65b
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 21:46:50 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count =0;
+
+  $: if(count>=10){
+    alert("the count is bigger than 10");
+    count =9;
+  }
+
+  function handleClick(){
+    count += 1;
+  }
+</script>
+<h1>reactivity/ statements</h1>
+<button on:click={handleClick}>
+count {count} {count === 1 ? 'time':'times'}
+</button>
+<p>click more than 9x to see what happens</p>
